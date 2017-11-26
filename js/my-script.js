af3b3bd95138fe806d86ddd0c3560849105c9812
inat <sense.starts.with.knowledge@gmail.com>
2017-11-27 03:49:20 +0900

    almost done

--- js/my-script.js
+++ js/my-script.js
@@ -12,35 +12,12 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 $(function () {
-  var prof = $('#prof').height();
-  var work = $('#work').height();
-  var study = $('#study').height();
-  var footer = $('#footer').height();
   $(window).scroll(function () {
-    if ($(this).scrollTop() >= prof) {
+    if ($(this).scrollTop() >= $('header').height()) {
       $('h1').addClass('on');
-      $('header nav ul li:first-child').addClass('on');
     } else {
       $('h1').removeClass('on');
-      $('header nav ul li:first-child').removeClass('on');
     }
-
-    //
-    // if ($(this).scrollTop() >= work) {
-    //   $('header nav ul li:nth-child(2)').addClass('on');
-    //   $('header nav ul li:first-child').removeClass('on');
-    // } else {
-    //   $('header nav ul li:nth-child(2)').removeClass('on');
-    // }
-    //
-    // if ($(this).scrollTop() >= study) {
-    //   $('header nav ul li:last-child').addClass('on');
-    //   $('header nav ul li:nth-child(2)').removeClass('on');
-    // } else if ($(this).scrollTop() >= footer) {
-    //   $('header nav ul li:last-child').removeClass('on');
-    // } else {
-    //   $('header nav ul li:last-child').removeClass('on');
-    // }
   });
 
   $('.top_btn a').click(function () {
@@ -49,4 +26,6 @@ $(function () {
       }, 2000);
       return false;
     });
+
+  $('header').prepend('<div class="layer"></div>');
 });
